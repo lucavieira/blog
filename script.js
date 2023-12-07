128d6a92794d6ade358a3f6598164609b53a0c57
lucavieira <lukasveras14@gmail.com>
2023-12-07 09:14:27 -0300

    Ajustes na função Like. Criação de novo post adicionado.

--- script.js
+++ script.js
@@ -1,9 +1,74 @@
-let likeButton = document.querySelector('.botao-gostei')
+let newPostButton = document.querySelector('#novo-post')
+let main = document.querySelector('main')
 
-likeButton.addEventListener('click', () => {
+const months = ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez']
+
+newPostButton.addEventListener('click', newPost)
+
+function like(id) {
+    let likeButton = document.getElementById(`${id}`)
     if(likeButton.getAttribute('src') == './assets/images/heart-icon.svg') {
         likeButton.setAttribute('src', './assets/images/heart-icon-colored.svg')
     } else {
         likeButton.setAttribute('src', './assets/images/heart-icon.svg')
     }
-})
+}
+
+function newPost() {
+    let postData = `${new Date().getDate()} de ${months[new Date().getMonth()]}, ${new Date().getFullYear()}`
+    let postTitle = prompt('Titulo da Postagem: ')
+    let postDescription = prompt('Descrição da Postagem: ')
+
+    let post = document.createElement('div')
+    post.classList.add('posts')
+
+    // Informações
+    let information = document.createElement('div')
+    information.classList.add('informacoes')
+
+    let createdData = document.createElement('small')
+    createdData.classList.add('data-publicacao')
+    createdData.innerHTML = postData
+
+    let heartButton = document.createElement('img')
+    heartButton.setAttribute('src', './assets/images/heart-icon.svg')
+    heartButton.classList.add('botao-gostei')
+    heartButton.id = randomID()
+    heartButton.onclick = () => like(heartButton.id)
+
+    information.appendChild(createdData)
+    information.appendChild(heartButton)
+
+    // Conteúdo
+    let content = document.createElement('div')
+    content.classList.add('conteudo')
+
+    let title = document.createElement('h2')
+    title.classList.add('titulo-post')
+    title.innerHTML = postTitle
+
+    let description = document.createElement('p')
+    description.classList.add('descricao-post')
+    description.innerHTML = postDescription
+
+    content.appendChild(title)
+    content.appendChild(description)
+
+    post.appendChild(information)
+    post.appendChild(content)
+
+    main.appendChild(post)
+}
+
+function randomID(min=0, max=5000) {
+    let listButtons = document.getElementsByClassName('botao-gostei')
+    let id = Math.floor(Math.random() * (max - min) + min)
+
+    for(let i = 0; i < listButtons.length; i++) {
+        if(listButtons[i].id != id) {
+            return id
+        } else {
+            randomID()
+        }
+    }
+}
